f099d5ebb3ac9c07c7c01a57bb7a2b8996d6e66e
zoodfekr <zoodfekr.r@gmail.com>
2024-07-18 12:06:13 +0330

    use context and condition

--- home/views.py
+++ home/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+data={
+    "name":"django app",
+    "value":20
+
+}
+
 def homeview(request):
-    return render(request,'home.html')
+    return render(request,'home.html' ,context=data )
