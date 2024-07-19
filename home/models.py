bd68cc3c9ee229181a4eeb158435f5e93459ce6d
Ramin <100441714+zoodfekr@users.noreply.github.com>
2024-07-19 21:25:14 +0330

    creat model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class World (models.Model):
+    world: models.CharField(max_length=100)
+    mean: models.CharField(max_length=100)
+    created:models.DateTimeField()
--- home/views.py
+++ home/views.py
@@ -3,11 +3,27 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-data={
-    "name":"django app",
-    "value":20
+data={"name":"django app", "value":20}
 
+fruits = ['apple', 'Banana', 'Cherry', 'Orange','Banana']
+
+class_value={"text"}
+
+firstof_test = {
+'val1': '',
+'val2': '',
+'val3': 'Hello, World!'
 }
 
+
 def homeview(request):
-    return render(request,'home.html' ,context=data )
+    return render(
+        request,
+        'home.html',
+        context={
+            "data":data ,
+            "fruits":fruits,
+            "class_value":class_value,
+            "firstof_test":firstof_test
+        }
+    )
