d6da2c1c1349393196fb9c79eb457b7c2127b973
Ramin <100441714+zoodfekr@users.noreply.github.com>
2024-07-28 14:11:43 +0330

    use include and extends

--- home/views.py
+++ home/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
-
-data={"name":"django app", "value":20}
-
-fruits = ['apple', 'Banana', 'Cherry', 'Orange','Banana']
-
-class_value={"text"}
-
-firstof_test = {
-'val1': '',
-'val2': '',
-'val3': 'Hello, World!'
-}
-
+from .models import Todo;
 
 def homeview(request):
+    data = Todo.objects.all()
     return render(
         request,
         'home/home.html',
-        context={
-            "data":data ,
-            "fruits":fruits,
-            "class_value":class_value,
-            "firstof_test":firstof_test
-        }
+        context={"data":data }
     )
